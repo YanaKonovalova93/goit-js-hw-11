aea036acfc8e629fbc47307baaf756fde0b6947d
YanaKonovalova93 <mikobacteria@gmail.com>
2023-05-18 17:22:10 +0300

    start project2

--- src/refs.js
+++
@@ -1,3 +0,0 @@
-export const inputEl = document.getElementById('search-box');
-export const divCountryEl = document.querySelector('.country-info');
-export const listCountryEl = document.querySelector('.country-list');
